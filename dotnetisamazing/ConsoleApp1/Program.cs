93aa4e93296d04ccca3f34a5299a2f19168e5cc8
s30174 <s30174@pjwstk.edu.pl>
2025-03-03 15:07:01 +0100

    wrote avg feature

--- dotnetisamazing/ConsoleApp1/Program.cs
+++ dotnetisamazing/ConsoleApp1/Program.cs
@@ -3,3 +3,15 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("I hate microsoft");
 Console.WriteLine("Please give me arch laptop i'm tired");
 Console.WriteLine("S3RL - R4V3 BOY");
+
+
+static int getAvg(int[] arr)
+{
+    int avg = 0;
+    for (int i = 0; i < arr.Length; i++) {
+        avg += arr[i];
+    }
+    return avg/arr.Length;
+}
+
+Console.WriteLine(getAvg(new int[]{1,2,3,4}));
