8de7f0dc6ee58389aa01f22f2cb0f09d5f8dbd2c
s30174 <s30174@pjwstk.edu.pl>
2025-03-03 14:56:49 +0100

    added my personal info

--- dotnetisamazing/ConsoleApp1/Program.cs
+++ dotnetisamazing/ConsoleApp1/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("I hate microsoft");
+Console.WriteLine("Please give me arch laptop i'm tired");
